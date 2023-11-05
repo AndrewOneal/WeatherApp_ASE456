37a1ba00eb2743e7dfd1b996d49fe6f945b2c2a5
brycebien <brycebien@gmail.com>
2023-11-05 15:24:27 -0500

    started on Check for notifications
    commented out test code used to ensure the API was being called correctly

---
+++ lib/app/services/checkNotification.dart
@@ -0,0 +1,46 @@
+import 'weather.dart';
+
+main() async {
+  CheckLocation location = CheckLocation();
+  await location.checkEmergency();
+}
+
+abstract class Check {
+  static final WeatherModel weather = WeatherModel();
+  Future<dynamic> checkEmergency();
+  Future<dynamic> checkAlert();
+}
+
+class CheckLocation extends Check {
+  var weatherData;
+  CheckLocation() {
+    weatherData = null;
+    String message = '';
+    bool isEmergency = false;
+  }
+
+  Future<dynamic> fetchData() async {
+    weatherData = await Check.weather.getLocationWeather();
+  }
+
+  @override
+  checkEmergency() async {
+    await fetchData();
+
+    //List of emergencies to detect:
+    //wind gust > 40mph
+    //temp > 100F
+    // >50mm of rainfall/snow
+    // visibility < 100m
+    if (weatherData['main']['temp'] > 37.78) {
+      print('there will be an emergency');
+      //append emergency string to message
+      //set isEmergency to True
+    }
+  }
+
+  @override
+  checkAlert() async {
+    print('check Alert');
+  }
+}
