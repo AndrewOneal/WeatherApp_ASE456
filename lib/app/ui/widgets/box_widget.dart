74b30a71ce4ba389ba79a882b879b5e149cd9bbc
Lulseged Admasu <admasul1@mymail.nku.edu>
2023-11-09 16:47:33 -0500

    completed page transitions on btn click | compelted loading state on app init and search submit | dynamic height set by children except for two boxes |  width set by container parent /column for all children

--- lib/app/ui/widgets/box_widget.dart
+++ lib/app/ui/widgets/box_widget.dart
@@ -5,13 +5,18 @@ class BoxWidget extends StatelessWidget {
   final Color color;
   final BorderRadius? borderRadius;
   final Border? border;
+  final double height;
 
   BoxWidget(
-      {this.color = KThemeColors.bg_darkBlue, this.borderRadius, this.border});
+      {this.color = KThemeColors.bg_darkBlue,
+      this.borderRadius,
+      this.border,
+      this.height = 400});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: height,
       decoration: BoxDecoration(
           color: color, borderRadius: borderRadius, border: border),
     );
--- lib/app/ui/widgets/top.dart
+++ lib/app/ui/widgets/top.dart
@@ -3,14 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Top extends StatelessWidget {
+  final Function onSearch;
+  final Function onNavigateToPage1;
+  final Function onNavigateToPage2;
+
+  Top({
+    required this.onSearch,
+    required this.onNavigateToPage1,
+    required this.onNavigateToPage2,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 80.0,
       padding: EdgeInsets.all(20),
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [Logo(), SearchBar(), ButtonContainer()]),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Logo(),
+        SearchBar(onSearch: onSearch),
+        ButtonContainer(
+          onNavigateToPage1: onNavigateToPage1,
+          onNavigateToPage2: onNavigateToPage2,
+        )
+      ]),
     );
   }
 }
@@ -47,14 +62,19 @@ class Logo extends StatelessWidget {
 }
 
 class SearchBar extends StatelessWidget {
+  final Function onSearch;
+
+  SearchBar({required this.onSearch});
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(8.0),
       decoration: BoxDecoration(
-          color: Color(0xFF1E1E1E),
-          borderRadius: KThemeBorderRadius.borderRadius_sm,
-          border: KThemeBorders.border_md),
+        color: Color(0xFF1E1E1E),
+        borderRadius: KThemeBorderRadius.borderRadius_sm,
+        border: KThemeBorders.border_md,
+      ),
       child: Row(
         children: [
           Icon(
@@ -69,6 +89,9 @@ class SearchBar extends StatelessWidget {
               style: TextStyle(
                 color: Colors.white,
               ),
+              onSubmitted: (value) {
+                onSearch();
+              },
               decoration: InputDecoration(
                 hintText: "Search",
                 hintStyle: TextStyle(
@@ -78,6 +101,7 @@ class SearchBar extends StatelessWidget {
               ),
             ),
           ),
+          SizedBox(width: 8.0),
         ],
       ),
     );
@@ -85,32 +109,54 @@ class SearchBar extends StatelessWidget {
 }
 
 class ButtonContainer extends StatelessWidget {
+  final Function onNavigateToPage1;
+  final Function onNavigateToPage2;
+
+  ButtonContainer({
+    required this.onNavigateToPage1,
+    required this.onNavigateToPage2,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         _buildCircularIconButton(
-            context,
-            KThemeColors.theme_blue.withOpacity(.25),
-            Icons.location_on,
-            KThemeColors.theme_blue),
+          context,
+          KThemeColors.theme_blue.withOpacity(.25),
+          Icons.location_on,
+          KThemeColors.theme_blue,
+          () {
+            onNavigateToPage1();
+          },
+        ),
         SizedBox(width: 16.0),
-        _buildCircularIconButton(context, Color.fromRGBO(255, 255, 255, .23),
-            Icons.settings, KThemeColors.text_whiteWhite),
+        _buildCircularIconButton(
+          context,
+          Color.fromRGBO(255, 255, 255, .23),
+          Icons.settings,
+          KThemeColors.text_whiteWhite,
+          () {
+            onNavigateToPage2();
+          },
+        ),
       ],
     );
   }
 
   Widget _buildCircularIconButton(
-      BuildContext context, Color bgColor, IconData icon, Color iconColor) {
+    BuildContext context,
+    Color bgColor,
+    IconData icon,
+    Color iconColor,
+    VoidCallback onTap,
+  ) {
     return Material(
       shape: CircleBorder(),
       color: bgColor,
       child: InkWell(
         borderRadius: BorderRadius.circular(48.0),
-        onTap: () {
-          // Handle button tap
-        },
+        onTap: onTap,
         child: Container(
           width: 48.0,
           height: 48.0,
