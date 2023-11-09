221db0a6ecaa5e9391ce42c02bebe03036b84012
Lulseged Admasu <admasul1@mymail.nku.edu>
2023-11-09 16:55:07 -0500

    adding cloc-progress

--- lib/app/ui/screens/mock/page1.dart
+++ lib/app/ui/screens/mock/page1.dart
@@ -1,4 +1,3 @@
-// Page1.dart
 import 'package:flutter/material.dart';
 
 class Page1 extends StatelessWidget {
--- lib/app/ui/screens/mock/page2.dart
+++ lib/app/ui/screens/mock/page2.dart
@@ -1,4 +1,3 @@
-// Page2.dart
 import 'package:flutter/material.dart';
 
 class Page2 extends StatelessWidget {
